c2bea6984a0adb93df8a439b37366c5f7ae99c24
Cole Peterson <mail@colepeterson.me>
2025-08-21 18:28:41 -0400

    Added project-wide module, added more file exclusions, made localhost explicit in environment

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""A system for analyzing recipe structures and relationships between ingredients,
+equipment, and cooking actions. Provides tools for representing recipes as
+directed acyclic graphs and performing operations on them.
+"""
+
+__version__ = '0.1.0'
